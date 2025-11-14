aa49b0191c8d5df31c0b4072d5b6a483990922cc
Selasi Brown <selasiagorsor27@gmail.com>
2025-11-14 23:39:27 +0000

    Create wassce_grade.py

---
+++ wassce_grade.py
@@ -0,0 +1,17 @@
+score = int(input("What is the student's score: "))
+   
+    if score >= 80:
+        print("Student got a grade A, Excellent work")
+    elif score >= 70:
+        print("Student got a grade B, good work")
+    elif score >= 60:
+        print("Student got a grade C, more room for improvement")
+    elif score >= 50:
+        print("Student got a grade D, PASS")
+    elif score >= 40:
+        print("Student got a grade E, credit")
+    elif score <= 40:
+        print("Student got a grade F, fail!")
+    else
+        print("Student's grade unavailable")
+
